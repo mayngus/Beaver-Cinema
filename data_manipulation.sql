f5bb83a6b03de41795df985c9dd36bea7dd505bb
Geraldo HernandezGarcia <hernager@flip1.engr.oregonstate.edu>
2020-05-18 22:56:39 -0700

    add queries for search.php

--- data_manipulation.sql
+++ data_manipulation.sql
@@ -1,15 +1,18 @@
 -- Beaver Cinema DB
 --
--- Host: mysql.eecs.oregonstate.edu    Database: cs340_hernager
+-- Host: classmysql.oregonstate.edu    Database: cs340_***
 -- ------------------------------------------------------
--- Server version	
 
 /********************************************************
 *	Data manipulations for "search.php" page
 ********************************************************/
 
 -- get all information from movies using the 'movie search' form
-SELECT title, price, num_in_stock, movie_rating FROM Movies WHERE title = :title_ID_selected_from_search_bar
+SELECT title, price, num_stock, description FROM movie WHERE title = :title_ID_selected_from_search_bar
 
--- get all information from movies using the 'movie search' form
-SELECT title, price, num_in_stock, movie_rating FROM Movies WHERE title = :title_ID_selected_from_search_bar
+-- get all movie information from a searched up actor using actor's first name
+SELECT m.movie_id, m.title FROM m
+INNER JOIN movie_actor ON movie_actor.movie_id = movie.movie_id
+INNER JOIN actor ON actor.actor_id = movie_actor.actor_id
+WHERE actor.first_name = :actor_ID_selected_from_search_bar
+ORDER BY m.movie_id ASC;
