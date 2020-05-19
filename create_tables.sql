672b9cfa84f5c9a462066734f08954bfba599140
Benjamin Mayinger <mayingeb@oregonstate.edu>
2020-05-18 17:01:00 -0700

    Added example inserts

--- create_tables.sql
+++ create_tables.sql
@@ -14,6 +14,9 @@ CREATE TABLE Customer(
 
 );
 
+--example insert for Customer
+INSERT INTO Customer (cust_first_name, cust_last_name, cust_street, cust_city, cust_state, cust_zip, cust_phone, cust_email) VALUES ('Jimmy', 'John', '4134 Monroe Ave', 'Corvallis', 'OR', 97330, 5034150290, 'jjohn@gmail.com');
+
 CREATE TABLE Orders(
 
  orderID int AUTO_INCREMENT UNIQUE PRIMARY KEY,
@@ -26,12 +29,22 @@ CREATE TABLE Orders(
 
 );
 
+--example insert for Orders
+INSERT INTO Orders (customerID, credit_number, credit_exp, order_date, delivery) VALUES (
+    (SELECT customerID FROM Customer WHERE cust_first_name='Jimmy' AND cust_last_name='John'),
+    1234567812345678,
+    0824,
+    '2020-01-02',
+    'Delivery'
+);
+
+
 CREATE TABLE Movies(
 
  movieID int AUTO_INCREMENT UNIQUE PRIMARY KEY,
- orderID int,
- actorID int,
- genreID int,
+ orderID int DEFAULT NULL,
+ actorID int DEFAULT NULL,
+ genreID int DEFAULT NULL,
  price varchar(255) NOT NULL,
  num_in_stock int DEFAULT NULL,
  movie_rating int DEFAULT NULL,
@@ -41,6 +54,9 @@ CREATE TABLE Movies(
 
 );
 
+--example insert into Movies
+INSERT INTO Movies (price, num_in_stock, movie_rating) VALUES (4.95, 23, 4.8); 
+
 CREATE TABLE Actors(
 
  actorID int AUTO_INCREMENT NOT NULL UNIQUE PRIMARY KEY,
@@ -49,9 +65,17 @@ CREATE TABLE Actors(
 
 );
 
+--example insert into Actors
+INSERT INTO Actors (actor_name) VALUES ('Evelyn Hancock');
+
 CREATE TABLE Genres(
 
  movieID int AUTO_INCREMENT NOT NULL UNIQUE PRIMARY KEY,
  genre_type varchar(255) NOT NULL
 
 );
+
+--example insert into Genres
+INSERT INTO Actors (genre_type) VALUES ('Comedy');
+
+
