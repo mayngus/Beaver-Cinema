9bd8e349deaca3a5e4047979c2bbb14ef6dbb194
Geraldo HernandezGarcia <hernager@flip1.engr.oregonstate.edu>
2020-05-18 19:33:05 -0700

    fixed comments for table structure and data insertion

--- create_tables.sql
+++ create_tables.sql
@@ -1,3 +1,12 @@
+-- Beaver Cinema DB
+--
+-- Host: mysql.eecs.oregonstate.edu    Database: cs340_hernager
+-- ------------------------------------------------------
+-- Server version	
+
+--
+-- Table structure for table `Customer`
+--
 CREATE TABLE Customer(
 
  customerID int AUTO_INCREMENT UNIQUE PRIMARY KEY,
@@ -14,11 +23,16 @@ CREATE TABLE Customer(
 
 );
 
---example insert for Customer
-INSERT INTO Customer (cust_first_name, cust_last_name, cust_street, cust_city, cust_state, cust_zip, cust_phone, cust_email) VALUES ('Jimmy', 'John', '4134 Monroe Ave', 'Corvallis', 'OR', 97330, 5034150290, 'jjohn@gmail.com');
+--
+-- Dumping data for table `Customer`
+--
+INSERT INTO Customer (cust_first_name, cust_last_name, cust_street, cust_city, cust_state, cust_zip, cust_phone, cust_email) 
+VALUES ('Jimmy', 'John', '4134 Monroe Ave', 'Corvallis', 'OR', 97330, 5034150290, 'jjohn@gmail.com');
 
+--
+-- Table structure for table `Orders`
+--
 CREATE TABLE Orders(
-
  orderID int AUTO_INCREMENT UNIQUE PRIMARY KEY,
  customerID int AUTO_INCREMENT,
  credit_number int NOT NULL,
@@ -29,7 +43,9 @@ CREATE TABLE Orders(
 
 );
 
---example insert for Orders
+--
+-- Dumping data for table `Orders`
+--
 INSERT INTO Orders (customerID, credit_number, credit_exp, order_date, delivery) VALUES (
     (SELECT customerID FROM Customer WHERE cust_first_name='Jimmy' AND cust_last_name='John'),
     1234567812345678,
@@ -38,9 +54,10 @@ INSERT INTO Orders (customerID, credit_number, credit_exp, order_date, delivery)
     'Delivery'
 );
 
-
+--
+-- Table structure for table `Movies`
+--
 CREATE TABLE Movies(
-
  movieID int AUTO_INCREMENT UNIQUE PRIMARY KEY,
  orderID int DEFAULT NULL,
  actorID int DEFAULT NULL,
@@ -54,28 +71,37 @@ CREATE TABLE Movies(
 
 );
 
---example insert into Movies
+--
+-- Dumping data for table `Movies`
+--
 INSERT INTO Movies (price, num_in_stock, movie_rating) VALUES (4.95, 23, 4.8); 
 
-CREATE TABLE Actors(
 
+--
+-- Table structure for table `Actors`
+--
+CREATE TABLE Actors(
  actorID int AUTO_INCREMENT NOT NULL UNIQUE PRIMARY KEY,
  actor_name varchar(255) NOT NULL
-
-
 );
 
---example insert into Actors
+--
+-- Dumping data for table `Actors`
+--
 INSERT INTO Actors (actor_name) VALUES ('Evelyn Hancock');
 
+--
+-- Table structure for table `Genres`
+--
 CREATE TABLE Genres(
-
  movieID int AUTO_INCREMENT NOT NULL UNIQUE PRIMARY KEY,
  genre_type varchar(255) NOT NULL
 
 );
 
---example insert into Genres
-INSERT INTO Actors (genre_type) VALUES ('Comedy');
+--
+-- Dumping data for table `Genres`
+--
+INSERT INTO Genres (genre_type) VALUES ('Comedy');
 
 
