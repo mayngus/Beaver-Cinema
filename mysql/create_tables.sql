5b8a1992df1beb96e2d853ee81a966e1555508ef
Geraldo HernandezGarcia <hernager@flip3.engr.oregonstate.edu>
2020-05-26 10:19:49 -0700

    Fixed all tables and inserted values to relational tables

--- mysql/create_tables.sql
+++ mysql/create_tables.sql
@@ -17,7 +17,7 @@ CREATE TABLE `customer` (
 	`zip` int(11) unsigned NOT NULL,
 	`phone` varchar(50) DEFAULT NULL,
 	`email` varchar(50) DEFAULT NULL,
-	PRIMARY KEY (`customer_id`),
+	PRIMARY KEY (`customer_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 --
@@ -36,43 +36,44 @@ CREATE TABLE `orders` (
 	`customer_id` int(11) unsigned NOT NULL,
 	`credit_number` int(11) NOT NULL,
 	`credit_exp` int(11) NOT NULL,
-	`amount` int(11) NOT NULL,
 	`order_date` datetime NOT NULL,
-	`delivery` varchar(255) NOT NULL
+	`delivery` varchar(255) NOT NULL,
 	PRIMARY KEY (`order_id`),
-	CONSTRAINT `fk_order_customer` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON UPDATE CASCADE,
+	CONSTRAINT `fk_order_customer` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 --
 -- Insertion queries for table `orders`
 --
-INSERT INTO `orders` (customer_id, credit_number, credit_exp, order_date, delivery) VALUES (
-	(SELECT customer_id FROM customer WHERE first_name='Jimmy' AND last_name='John'),
-	1234567812345678,
-	0824,
-	'2020-01-02',
-	'Delivery'
-);
+LOCK TABLES `orders` WRITE;
+INSERT INTO `orders` VALUES (1, 1, 1234567812, 0824, '2020-01-02', 'Delivery'), (2, 5, 1234523456, 1024, '2020-03-25', 'Delivery'), (3, 2, 1867812345, 1222, '2020-04-15', 'Pickup'), (4, 3, 1863456778, 0622, '2020-03-22', 'Delivery'), (5, 4, 9412345678, 0323, '2020-01-29', 'Delivery');
+UNLOCK TABLES;
+-- INSERT INTO `orders` (customer_id, credit_number, credit_exp, order_date, delivery) VALUES (
+-- 	(SELECT customer_id FROM customer WHERE first_name='Jimmy' AND last_name='John'),
+-- 	1234567812345678,
+-- 	0824,
+-- 	'2020-01-02',
+-- 	'Delivery'
+-- );
 
 --
--- Table structure for table `movie`
+-- Table structure for table `movies`
 --
-DROP TABLE IF EXISTS `movie`;
-CREATE TABLE `movie` (
+DROP TABLE IF EXISTS `movies`;
+CREATE TABLE `movies` (
 	`movie_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
 	`title` varchar(255) NOT NULL,
 	`price` decimal(5,2) NOT NULL DEFAULT '5.99',
 	`num_stock` int(11) unsigned DEFAULT NULL,
 	`movie_description` text,
-	PRIMARY KEY (`movie_id`),
-	CONSTRAINT `fk_movie_order` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`) ON UPDATE CASCADE,
+	PRIMARY KEY (`movie_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=utf8;
 
 --
--- Insertion queries for table `movie`
+-- Insertion queries for table `movies`
 --
-LOCK TABLES `movie` WRITE;
-INSERT INTO `movie` VALUES (1, 'Contagion', 7.99, 20, 'A virus unleashes onto an unprepared world.'), (2, 'The Happening', 5.99, 6, 'A science teacher survives a pleague with a twist at the end'), (3, 'Warm Bodies', 9.00, 20, 'A zombie outbreak with a hint of heart and romance.'), (4, 'The Day After Tomorrow', 10.00, 15, 'A father searches for his son amidst a global weather disaster'), (5, 'World War Z', 6.50, 10, 'Brad Pitt investigates a deadly global virus that turns its victims into monsters'), (6, 'Andromeda Strain', 6.99, 12, 'A group of scientists investigate a deadl virus in a small town'), (7, 'Quarantine', 10.00, 20, 'A TV reporter is trapped in a building with an infectious disease'), (8, 'I Am Legend', 12.00, 25, 'A man and his dog attempt to find a cure to a deadly global outbreak'), (9, 'It Comes At Night', 2.99, 4, 'A family surviving an outbreak in the woods get a surprising visitor'), (10, 'Outbreak', 4.99, 15, 'A team of experts race around the clock to find a cure for an airbone virus'), (11, 'The Godfather', 7.99, 10, 'A sons ascension to his fathers mafia throne in New York City.'), (12, 'The Fellowship of the Ring', 6.99, 12, 'A young hobbit goes on a journey to save the world with an axe by his side.'), (12, 'Interstellar', 9.99, 14, 'A farmer goes on a voyage across the stars to solve the mystery of gravity.'), (13, 'Icarus', 1.99, 6, 'A doping documentary takes a twist into a massive international scandal'), (14, 'Jumanji', 4.99, 5, 'A shoe factory boy finds a mysterious board game filled with monsters'), (15, 'The Hangover', 7.99, 9, 'A bachelor party in Las Vegas is filled with amnesia for this trio');
+LOCK TABLES `movies` WRITE;
+INSERT INTO `movies` VALUES (1, 'Contagion', 7.99, 20, 'A virus unleashes onto an unprepared world.'), (2, 'The Happening', 5.99, 6, 'A science teacher survives a pleague with a twist at the end'), (3, 'Warm Bodies', 9.00, 20, 'A zombie outbreak with a hint of heart and romance.'), (4, 'The Day After Tomorrow', 10.00, 15, 'A father searches for his son amidst a global weather disaster'), (5, 'World War Z', 6.50, 10, 'Brad Pitt investigates a deadly global virus that turns its victims into monsters'), (6, 'Andromeda Strain', 6.99, 12, 'A group of scientists investigate a deadl virus in a small town'), (7, 'Quarantine', 10.00, 20, 'A TV reporter is trapped in a building with an infectious disease'), (8, 'I Am Legend', 12.00, 25, 'A man and his dog attempt to find a cure to a deadly global outbreak'), (9, 'It Comes At Night', 2.99, 4, 'A family surviving an outbreak in the woods get a surprising visitor'), (10, 'Outbreak', 4.99, 15, 'A team of experts race around the clock to find a cure for an airbone virus'), (11, 'The Godfather', 7.99, 10, 'A sons ascension to his fathers mafia throne in New York City.'), (12, 'The Fellowship of the Ring', 6.99, 12, 'A young hobbit goes on a journey to save the world with an axe by his side.'), (13, 'Interstellar', 9.99, 14, 'A farmer goes on a voyage across the stars to solve the mystery of gravity.'), (14, 'Icarus', 1.99, 6, 'A doping documentary takes a twist into a massive international scandal'), (15, 'Jumanji', 4.99, 5, 'A shoe factory boy finds a mysterious board game filled with monsters'), (16, 'The Hangover', 7.99, 9, 'A bachelor party in Las Vegas is filled with amnesia for this trio');
 UNLOCK TABLES;
 
 --
@@ -82,17 +83,17 @@ DROP TABLE IF EXISTS `movie_orders`;
 CREATE TABLE `movie_orders` (
 	`movie_id` int(11) unsigned NOT NULL,
 	`order_id` int(11) unsigned NOT NULL,
-	`quantity_ordered` int (11) unsigned NOT NULL 
+	`quantity_ordered` int (11) unsigned NOT NULL,
 	PRIMARY KEY (`movie_id`,`order_id`),
-	CONSTRAINT `fk_movie_order_movie` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`movie_id`) ON UPDATE CASCADE
-	CONSTRAINT `fk_order_movie_order` FOREIGN KEY (`order_id`) REFERENCES `order` (`order_id`) ON UPDATE CASCADE,
+	CONSTRAINT `fk_movie_orders_movie` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`movie_id`) ON UPDATE CASCADE,
+	CONSTRAINT `fk_movie_orders_order` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Insertion queries for table `movie_orders`
 --
 LOCK TABLES `movie_orders` WRITE;
-INSERT INTO `movie_orders` VALUES (1,1),(1,2);
+INSERT INTO `movie_orders` VALUES (1,1,2),(2,4,2),(8,3,3),(15,2,3),(12,5,1);
 UNLOCK TABLES;
 
 --
@@ -111,7 +112,7 @@ CREATE TABLE `actor` (
 -- Insertion queries for table `actor`
 --
 LOCK TABLES `actor` WRITE;
-INSERT INTO `actor` VALUES (1,'Evelyn','Hancock');
+INSERT INTO `actor` VALUES (1,'Brad','Pitt'),(2,'Matt', 'Damon'),(3, 'Dustin', 'Hoffman'),(4, 'Nicholas', 'Hoult');
 UNLOCK TABLES;
 
 --
@@ -120,34 +121,34 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `movie_actors`;
 CREATE TABLE `movie_actors` (
 	`movie_id` int(11) unsigned NOT NULL,
-	`order_id` int(11) unsigned NOT NULL,
-	PRIMARY KEY (`movie_id`,`order_id`),
-	CONSTRAINT `fk_movie_actor_movie` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`movie_id`) ON UPDATE CASCADE
-	CONSTRAINT `fk_actor_movie_actor` FOREIGN KEY (`actor_id`) REFERENCES `actor` (`actor_id`) ON UPDATE CASCADE,
+	`actor_id` int(11) unsigned NOT NULL,
+	PRIMARY KEY (`movie_id`,`actor_id`),
+	CONSTRAINT `fk_movie_actors_movie` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`movie_id`) ON UPDATE CASCADE,
+	CONSTRAINT `fk_movie_actors_actor` FOREIGN KEY (`actor_id`) REFERENCES `actor` (`actor_id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Insertion queries for table `movie_actors`
 --
 LOCK TABLES `movie_actors` WRITE;
-INSERT INTO `movie_actors` VALUES (1,1), (1,2);
+-- INSERT INTO
 UNLOCK TABLES;
 
 --
--- Table structure for table `genre`
+-- Table structure for table `genres`
 --
-DROP TABLE IF EXISTS `genre`;
-CREATE TABLE `genre` (
-	`genre_id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `genres`;
+CREATE TABLE `genres` (
+	`genre_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
 	`genre_name` varchar(25) NOT NULL,
 	PRIMARY KEY (`genre_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=300 DEFAULT CHARSET=utf8;
 
 --
--- Insertion queries for table `genre`
+-- Insertion queries for table `genres`
 --
-LOCK TABLES `genre` WRITE;
-INSERT INTO `genre` VALUES (1,'Action'),(2,'Comedy'),(3,'Science Fiction'),(4, 'Horror'),(5,'Romance'),(6,'Adventure'),(7,'Documentary'),(8,'Drama'), (9,'Fantasy');
+LOCK TABLES `genres` WRITE;
+INSERT INTO `genres` VALUES (1,'Action'),(2,'Comedy'),(3,'Science Fiction'),(4, 'Horror'),(5,'Romance'),(6,'Adventure'),(7,'Documentary'),(8,'Drama'), (9,'Fantasy');
 UNLOCK TABLES;
 
 --
@@ -158,13 +159,13 @@ CREATE TABLE `movie_genres` (
 	`movie_id` int(11) unsigned NOT NULL,
 	`genre_id` int(11) unsigned NOT NULL,
 	PRIMARY KEY (`movie_id`,`genre_id`),
-	CONSTRAINT `fk_movie_genre_movie` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`movie_id`) ON UPDATE CASCADE
-	CONSTRAINT `fk_genre_movie_genre` FOREIGN KEY (`genre_id`) REFERENCES `genre` (`genre_id`) ON UPDATE CASCADE,
+	CONSTRAINT `fk_movie_genres_movie` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`movie_id`) ON UPDATE CASCADE,
+	CONSTRAINT `fk_movie_genres_genre` FOREIGN KEY (`genre_id`) REFERENCES `genres` (`genre_id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Insertion queries for table `movie_genres`
 --
 LOCK TABLES `movie_genres` WRITE;
-INSERT INTO `movie_genres` VALUES (1,1), (1,2);
+-- INSERT INTO 
 UNLOCK TABLES;
