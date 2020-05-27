285b1b1fe3b1818d1aa02479f9171762c950e9a6
Benjamin Mayinger <mayingeb@oregonstate.edu>
2020-05-26 21:58:37 -0700

    Fixed typo in insert.php

--- views/insert.php
+++ views/insert.php
@@ -6,10 +6,10 @@
 	$title = mysqli_real_escape_string($conn, $_REQUEST['title']);
 	$price = mysqli_real_escape_string($conn, $_REQUEST['price']);
 	$num_stock = mysqli_real_escape_string($conn, $_REQUEST['num_stock']);
-	$movie_descripton = mysqli_real_escape_string($conn, $_REQUEST['movie_descripton']);
+	$movie_descripton = mysqli_real_escape_string($conn, $_REQUEST['movie_description']);
 
-	$sql = "INSERT INTO Movies (title, price, num_stock, movie_descripton) 
-			VALUES ('$title', '$price', '$num_stock', '$movie_descripton')";
+	$sql = "INSERT INTO Movies (title, price, num_stock, movie_description) 
+			VALUES ('$title', '$price', '$num_stock', '$movie_description')";
 	if(mysqli_query($conn, $sql)){
 		echo "Movie added successfully.";
 	} 
