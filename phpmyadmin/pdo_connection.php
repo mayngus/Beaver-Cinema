ab22d6d7c4a64c5b23e5caf2782a6b68733ca592
Geraldo HernandezGarcia <hernager@flip2.engr.oregonstate.edu>
2020-05-26 20:48:23 -0700

    Added connections folder

---
+++ phpmyadmin/pdo_connection.php
@@ -0,0 +1,34 @@
+<?php
+
+try {
+    $dbhost = "classmysql.engr.oregonstate.edu";
+    $dbuser = "cs340_hernager";
+    $dbpass = "6862";
+    $db = "cs340_hernager";
+
+    // create instance of PDO and pass MySQL info to constructor
+    $dbh = new PDO("mysql:host=$dbhost;dbname=$db", $dbuser, $dbpass);
+    // force an Exception to display successful connection
+    $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+} catch (Exception $e) {
+    echo "Unable to connect";
+    // call Exception method to get the error message
+    echo $e->getMessage();
+    exit;
+}
+
+try {
+    // run statement you want to run
+    $query = 'SELECT title, price FROM movies';
+    $results = $dbh->query($query);
+
+} catch (Exception $e) {
+    echo "Unable to retrieve results";
+    exit;
+}
+
+var_dump($results->fetchAll(PDO::FETCH_ASSOC));
+
+?>
+
