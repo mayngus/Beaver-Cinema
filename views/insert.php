92dfb9d917819a6913a7bb4654b7d4ca126e0808
Benjamin Mayinger <mayingeb@oregonstate.edu>
2020-05-26 22:03:33 -0700

    Whoops, missed a character

--- views/insert.php
+++ views/insert.php
@@ -3,10 +3,10 @@
 	include 'db_connection.php';
 	$conn = OpenCon();
 
-	$title = POST['title'];
-	$price = POST['price'];
-	$num_stock = POST['num_stock'];
-	$movie_descripton = POST['movie_description'];
+	$title = $POST['title'];
+	$price = $POST['price'];
+	$num_stock = $POST['num_stock'];
+	$movie_descripton = $POST['movie_description'];
 
 	$sql = "INSERT INTO Movies (title, price, num_stock, movie_description) 
 			VALUES ('$title', '$price', '$num_stock', '$movie_description')";
