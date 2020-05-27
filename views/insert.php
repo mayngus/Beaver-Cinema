0d630f3e43837251b30b8f38c3f2c4e6d24b4378
Benjamin Mayinger <mayingeb@oregonstate.edu>
2020-05-26 22:01:57 -0700

    Changed REQUEST to POST

--- views/insert.php
+++ views/insert.php
@@ -3,10 +3,10 @@
 	include 'db_connection.php';
 	$conn = OpenCon();
 
-	$title = mysqli_real_escape_string($conn, $_REQUEST['title']);
-	$price = mysqli_real_escape_string($conn, $_REQUEST['price']);
-	$num_stock = mysqli_real_escape_string($conn, $_REQUEST['num_stock']);
-	$movie_descripton = mysqli_real_escape_string($conn, $_REQUEST['movie_description']);
+	$title = POST['title'];
+	$price = POST['price'];
+	$num_stock = POST['num_stock'];
+	$movie_descripton = POST['movie_description'];
 
 	$sql = "INSERT INTO Movies (title, price, num_stock, movie_description) 
 			VALUES ('$title', '$price', '$num_stock', '$movie_description')";
